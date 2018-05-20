1840f1a6a23d65998386705f3ddf24e4755a510a
Ahmed Mujtaba <mjtbahmed20@hotmail.com>
2018-05-21 00:08:57 +0530

    Add project page

--- server.js
+++ server.js
@@ -50,6 +50,11 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
 app.get('/bad', (req, res) => {
   res.send({
     error: 'Bad Request'
